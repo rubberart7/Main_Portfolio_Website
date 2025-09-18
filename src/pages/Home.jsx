3cd75b2156f153d2a71ac366006d3944cffe8e10
mbyaf <rubberart7@users.noreply.github.com>
2025-09-17 22:22:18 -0400

    project code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import { Navbar } from "../components/Navbar";
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "@/components/StarBackground";
+import { HeroSection } from "../components/HeroSection";
+import { AboutSection } from "../components/AboutSection";
+import { SkillsSection } from "../components/SkillsSection";
+import { ProjectsSection } from "../components/ProjectsSection";
+import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
 
-const Home = () => {
+export const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+      {/* Theme Toggle */}
+      <ThemeToggle />
+      {/* Background Effects */}
+      <StarBackground />
 
-export default Home
+      {/* Navbar */}
+      <Navbar />
+      {/* Main Content */}
+      <main>
+        <HeroSection />
+        <AboutSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <ContactSection />
+      </main>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+};
